31925d4260e44fa63d5c31bb4e48f09b2757c6d0
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-10-29 14:11:36 -0500

    Added frontend functionality for modal and validation for modal using vueform

--- src/components/education/EducationModal.vue
+++ src/components/education/EducationModal.vue
@@ -7,14 +7,107 @@ const { isVisible } = storeToRefs(modalStore);
 const closeDialog = () => {
   isVisible.value = !isVisible.value;
 };
+
+const degreeItems = ["Masters", "Bachelors", "Associates"];
+
+const submitForm = async (form$) => {
+  const data = form$.data;
+
+  console.log(data);
+};
 </script>
 <template>
-  <v-dialog v-model="isVisible">
-    <v-card :color="'#D9D9D9'" variant="tonal">
-      <v-card-item><v-card-title>Education</v-card-title> </v-card-item>
-      <v-card-actions>
-        <v-btn @click="closeDialog" color="primary">Close</v-btn>
-      </v-card-actions>
+  <v-dialog v-model="isVisible" max-width="60%">
+    <v-card
+      style="background-color: #d9d9d9; border-radius: 10px; padding: 20px"
+    >
+      <Vueform
+        size="md"
+        :endpoint="false"
+        @submit="submitForm"
+        :display-errors="false"
+      >
+        <StaticElement
+          name="education_title"
+          content="Education"
+          tag="h1"
+          align="center"
+        ></StaticElement>
+        <TextElement
+          name="institution"
+          before="Institution"
+          :rules="['required']"
+        />
+        <SelectElement
+          name="degree"
+          :items="degreeItems"
+          :native="false"
+          before="Degree"
+          :rules="['required']"
+        ></SelectElement>
+        <GroupElement
+          name="date_container"
+          description="Dates attended institution"
+        >
+          <DateElement
+            name="dateFrom"
+            :columns="{
+              container: 6,
+              label: 12,
+              wrapper: 12,
+            }"
+            before="Date From"
+            :rules="['required']"
+          ></DateElement>
+          <DateElement
+            name="dateTo"
+            :columns="{
+              container: 6,
+              label: 12,
+              wrapper: 12,
+            }"
+            before="Date To"
+            :rules="['required']"
+          ></DateElement>
+        </GroupElement>
+        <TextElement
+          name="gpa"
+          add-class="gpa-field"
+          before="GPA"
+        ></TextElement>
+        <TextareaElement
+          name="coursework"
+          before="Course Work"
+        ></TextareaElement>
+        <GroupElement name="button_container">
+          <ButtonElement
+            secondary="true"
+            name="cancel"
+            :submits="false"
+            button-label="Cancel"
+            :full="true"
+            size="md"
+            :columns="{
+              container: 6,
+              label: 12,
+              wrapper: 12,
+            }"
+            @click="closeDialog"
+          />
+          <ButtonElement
+            name="Submit"
+            :submits="true"
+            button-label="Create account"
+            :full="true"
+            size="md"
+            :columns="{
+              container: 6,
+              label: 12,
+              wrapper: 12,
+            }"
+          />
+        </GroupElement>
+      </Vueform>
     </v-card>
   </v-dialog>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import router from "./router.js";
 import vuetify from "./plugins/vuetify.js";
 import { createPinia } from "pinia";
 
+import vueformConfig from "../vueform.config.js";
+import Vueform from "@vueform/vueform";
+
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
 app.use(vuetify);
 app.use(pinia);
+app.use(Vueform, vueformConfig);
 app.mount("#app");
