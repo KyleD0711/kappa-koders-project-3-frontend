7da54610e0a521907e16a482c3edd174d167a489
Yorsh <george.hartman@eagles.oc.edu>
2024-11-05 10:28:23 -0600

    Adding more to side bar

--- src/views/Information/InformationList.vue
+++ src/views/Information/InformationList.vue
@@ -1,12 +1,15 @@
 <script setup>
 import SkillView from "../../components/skill/SkillView.vue";
+import SideBar from '../../components/InformationSideBar.vue'
+
 </script>
+
 <template>
+  <SideBar></SideBar>
   <SkillView></SkillView>
 </template>
 
 <script setup>
 
-import SideBar from '../../components/InformationSideBar.vue'
 
 </script>
