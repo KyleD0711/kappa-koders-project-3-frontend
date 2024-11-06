4da27e1b999be601b449bb03dfc926ddc4938d58
jonahveit2002 <97561214+jonahveit2002@users.noreply.github.com>
2024-11-06 09:58:16 -0600

    Sidebar added (need to make it dynamic)

---
+++ src/components/resume/ResumeSidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-navigation-drawer>
+        <v-list-item title="{Resume Name}"></v-list-item>
+
+        <v-divider></v-divider>
+        
+        <v-expansion-panels>
+          <v-expansion-panel title="Education"> </v-expansion-panel>
+        </v-expansion-panels>
+
+        <v-expansion-panels>
+          <v-expansion-panel title="Experience"> </v-expansion-panel>
+        </v-expansion-panels>
+
+        <v-expansion-panels>
+          <v-expansion-panel title="Personal Links"> </v-expansion-panel>
+        </v-expansion-panels>
+        
+        <v-expansion-panels>
+          <v-expansion-panel title="Honors/Awards"> </v-expansion-panel>
+        </v-expansion-panels>
+
+        <v-expansion-panels>
+          <v-expansion-panel title="Interests"> </v-expansion-panel>
+        </v-expansion-panels>
+
+      </v-navigation-drawer>
+    </v-container>
+  </v-app>
+</template>
--- src/views/Resume/ResumeList.vue
+++ src/views/Resume/ResumeList.vue
@@ -1,6 +1,8 @@
 <script setup>
 import ResumeViewer from "../../components/resume/ResumeViewer.vue";
+import ResumeSidebar from "../../components/resume/ResumeSidebar.vue";
 </script>
 <template>
+  <ResumeSidebar></ResumeSidebar>
   <ResumeViewer></ResumeViewer>
 </template>
