f74773d842706dfa86746c232bafb55fb67a1962
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-11-13 09:36:31 -0600

    Changed the PDFViewer to use props

--- src/components/resume/ResumeViewer.vue
+++ src/components/resume/ResumeViewer.vue
@@ -2,124 +2,42 @@
 import { onMounted, ref } from "vue";
 import htmlGenerator from "./resumeUtils/htmlGenerator";
 import jsonUtils from "./resumeUtils/jsonUtils";
-import template from "../../../templates/templates.json";
 
 const innerHtml = ref(null);
 
-// const props = defineProps({
-//   resume_data: {
-//     type: Object,
-//     required: true,
-//   },
-//   metadata: {
-//     type: Object,
-//     required: true,
-//   },
-//   header_data: {
-//     type: Object,
-//     required: true,
-//   },
-//   template: {
-//     type: Object,
-//     required: true,
-//   },
-// });
-
-const templateName = "template1";
-
-const metadata = {
-  render_fields: ["awards", "education", "project", "skills"],
-  section_dividers: true,
-};
-const header_data = {
-  title: "Kyle Denney",
-  email: "kyle.denney@email.com",
-  phone_number: "555-123-4567",
-  professional_summary:
-    "Bachelor of Arts degree candidate, with a major in Economics, and experience developing and analyzing cost models, providing quality assurance reviews, and creating process solutions to improve financial forecasts for clients. Looking to continue the development of risk management, audit, and compliance skills in a team-centered environment.",
-  links: [
-    {
-      name: "Portfolio",
-      url: "https://kyledenneyportfolio.com",
-    },
-  ],
-};
-const resume_data = {
-  education: [
-    {
-      institution: "University of State",
-      credential_earned: "Bachelor of Arts in Economics",
-      date_to: "2024-05",
-      date_from: "2020-08",
-      gpa: "3.8",
-      coursework: null,
-    },
-    {
-      institution: "University of State",
-      credential_earned: "Bachelor of Arts in Economics",
-      date_to: "2024-05",
-      date_from: "2020-08",
-      gpa: "3.8",
-      coursework: null,
-    },
-    {
-      institution: "University of State",
-      credential_earned: "Bachelor of Arts in Economics",
-      date_to: "2024-05",
-      date_from: "2020-08",
-      gpa: "3.8",
-      coursework: null,
-    },
-  ],
-  experience: [
-    {
-      employer: "ABC Financial Services",
-      position_title: "Financial Analyst Intern",
-      date_start: "2023-06",
-      date_end: "2023-08",
-    },
-    {
-      employer: "ABC Financial Services",
-      position_title: "Financial Analyst Intern",
-      date_start: "2023-06",
-      date_end: "2023-08",
-    },
-  ],
-  skills: [
-    {
-      name: "Data Analysis",
-      description:
-        "Proficient in Excel, R, and SQL for data analysis and financial modeling.",
-      proficiency_level: "Intermediate",
-    },
-  ],
-  project: [
-    {
-      name: "Cost Optimization Project",
-      description:
-        "Developed cost models to identify process efficiencies, resulting in a 10% cost reduction.",
-      date_start: "2023-01",
-      date_completed: "2023-04",
-    },
-  ],
-  awards: [
-    {
-      name: "Dean’s List",
-      institution: "University of State",
-      description: "Awarded for academic excellence in multiple semesters.",
-      date_awarded: "2022-12",
-    },
-  ],
-};
+const props = defineProps({
+  resume_data: {
+    type: Object,
+    required: true,
+  },
+  metadata: {
+    type: Object,
+    required: true,
+  },
+  header_data: {
+    type: Object,
+    required: true,
+  },
+  template: {
+    type: Object,
+    required: true,
+  },
+});
 
 const renderPDF = () => {
   let full_json_object = {};
-  let resume_template = template[templateName];
+
+  let header_data = props.header_data;
+  let metadata = props.metadata;
+  let resume_data = props.resume_data;
+  let template = props.template;
+
+  let title = `${header_data.fName} ${header_data.lName}`;
 
   let pdf_header = jsonUtils.findAndUpdateSectionByName(
-    resume_template.structure.pdf_header,
-    resume_template.data.pdf_header.title,
-    header_data.title
+    template.structure.pdf_header,
+    template.data.pdf_header.title,
+    title
   );
 
   let personal_info = `${header_data.email} | ${header_data.phone_number}`;
@@ -129,20 +47,20 @@ const renderPDF = () => {
 
   pdf_header = jsonUtils.findAndUpdateSectionByName(
     pdf_header,
-    resume_template.data.pdf_header.personal_info,
+    template.data.pdf_header.personal_info,
     personal_info
   );
 
   let professional_summary = jsonUtils.findAndUpdateSectionByName(
-    resume_template.structure.professional_summary,
-    resume_template.data.professional_summary,
+    template.structure.professional_summary,
+    template.data.professional_summary,
     header_data.professional_summary
   );
 
   let body_children = [pdf_header, professional_summary];
   metadata.render_fields.forEach((value) => {
     let section = jsonUtils.findAndUpdateSectionByData(
-      resume_template,
+      template,
       resume_data,
       value
     );
@@ -152,9 +70,8 @@ const renderPDF = () => {
   full_json_object = {
     resume: {
       section_name: "body",
-      content: "",
       style: {
-        "font-family": resume_template.structure.metadata.font,
+        "font-family": template.structure.metadata.font,
       },
       children: body_children,
     },
--- src/views/Resume/ResumeList.vue
+++ src/views/Resume/ResumeList.vue
@@ -1,28 +1,120 @@
 <script setup>
 import ResumeViewer from "../../components/resume/ResumeViewer.vue";
 import ResumeSidebar from "../../components/resume/ResumeSidebar.vue";
-import { ref } from 'vue';
+import { ref } from "vue";
+import template from "../../../templates/templates.json";
+const templateData = ref(template["template1"]);
 
-const resume_data = ref({});
-
+const metadata = ref({
+  render_fields: ["awards", "education", "project", "skills"],
+  section_dividers: true,
+});
+const header_data = ref({
+  fName: "Kyle",
+  lName: "Denney",
+  email: "kyle.denney@email.com",
+  phone_number: "555-123-4567",
+  professional_summary:
+    "Bachelor of Arts degree candidate, with a major in Economics, and experience developing and analyzing cost models, providing quality assurance reviews, and creating process solutions to improve financial forecasts for clients. Looking to continue the development of risk management, audit, and compliance skills in a team-centered environment.",
+  links: [
+    {
+      name: "Portfolio",
+      url: "https://kyledenneyportfolio.com",
+    },
+  ],
+});
+const resume_data = ref({
+  education: [
+    {
+      institution: "University of State",
+      credential_earned: "Bachelor of Arts in Economics",
+      date_to: "2024-05",
+      date_from: "2020-08",
+      gpa: "3.8",
+      coursework: null,
+    },
+    {
+      institution: "University of State",
+      credential_earned: "Bachelor of Arts in Economics",
+      date_to: "2024-05",
+      date_from: "2020-08",
+      gpa: "3.8",
+      coursework: null,
+    },
+    {
+      institution: "University of State",
+      credential_earned: "Bachelor of Arts in Economics",
+      date_to: "2024-05",
+      date_from: "2020-08",
+      gpa: "3.8",
+      coursework: null,
+    },
+  ],
+  experience: [
+    {
+      employer: "ABC Financial Services",
+      position_title: "Financial Analyst Intern",
+      date_start: "2023-06",
+      date_end: "2023-08",
+    },
+    {
+      employer: "ABC Financial Services",
+      position_title: "Financial Analyst Intern",
+      date_start: "2023-06",
+      date_end: "2023-08",
+    },
+  ],
+  skills: [
+    {
+      name: "Data Analysis",
+      description:
+        "Proficient in Excel, R, and SQL for data analysis and financial modeling.",
+      proficiency_level: "Intermediate",
+    },
+  ],
+  project: [
+    {
+      name: "Cost Optimization Project",
+      description:
+        "Developed cost models to identify process efficiencies, resulting in a 10% cost reduction.",
+      date_start: "2023-01",
+      date_completed: "2023-04",
+    },
+  ],
+  awards: [
+    {
+      name: "Dean’s List",
+      institution: "University of State",
+      description: "Awarded for academic excellence in multiple semesters.",
+      date_awarded: "2022-12",
+    },
+  ],
+});
 </script>
 <template>
-  <div style="display: flex;">
-    <div style="width: 50%;">
-      <ResumeSidebar :resume_data="resume_data"/>
+  <div style="display: flex">
+    <div style="width: 50%">
+      <ResumeSidebar :resume_data="resume_data" />
     </div>
-    <v-divider vertical :thickness="4" class="white border-opacity-100"></v-divider>
-    <div style="flex-grow: 1;">
-      <ResumeViewer :resume_data="resume_data"/>
+    <v-divider
+      vertical
+      :thickness="4"
+      class="white border-opacity-100"
+    ></v-divider>
+    <div style="flex-grow: 1">
+      <ResumeViewer
+        :template="templateData"
+        :metadata="metadata"
+        :header_data="header_data"
+        :resume_data="resume_data"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
-
-.white{
+.white {
   color: #737373;
   padding-left: 1%;
 }
-
 </style>
