c9d046377737e2381f1712db2585f0bb96c1c3a4
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-11-24 15:51:00 -0600

    Recursive shenanigans

---
+++ src/components/comment/Comment.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+import reviewServices from "../../services/reviewServices";
+import { onMounted } from "vue";
+
+const props = defineProps({
+  reviews: {
+    type: Array,
+    required: true,
+  },
+});
+
+const err = ref(false);
+
+const getReviews = async () => {
+  await reviewServices
+    .getReviewsForResume(props.resumeId)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+onMounted(async () => {
+  await getReviews();
+});
+</script>
+<template>
+  <div class="center-text">
+    <div v-if="!err">No reviews found!</div>
+    <div v-else class="error">
+      An error has occurred while trying to fetch your reviews!
+    </div>
+  </div>
+</template>
+<style scoped>
+.center-text {
+  display: flex; /* Use Flexbox */
+  justify-content: center; /* Horizontally center text */
+  align-items: center; /* Vertically center text */
+  height: 100%; /* Fill the parent container */
+  width: 100%; /* Fill the parent container */
+  text-align: center; /* Optional, ensures text is aligned if multi-line */
+}
+
+.error {
+  color: red;
+}
+</style>
---
+++ src/services/reviewServices.js
@@ -0,0 +1,12 @@
+import apiClient from "./services.js";
+export default {
+  getReview(reviewId) {
+    return apiClient.get(`/admin/resume/review/${reviewId}`);
+  },
+  updateReview(reviewId, review) {
+    return apiClient.put(`/admin/resume/review/${reviewId}`, review);
+  },
+  getReviewsForResume(resumeId) {
+    return apiClient.get(`/resume/${resumeId}/review`);
+  },
+};
