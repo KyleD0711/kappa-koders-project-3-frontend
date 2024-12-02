afbbabed6b5fd8d229709fa6a5c64ed1cbdfa05e
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-12-02 12:13:01 -0600

    Added parsing of description for experience and education so that line items can be added to the resume

--- src/components/education/EducationModal.vue
+++ src/components/education/EducationModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineEmits } from "vue";
+import { ref } from "vue";
 import educationServices from "../../services/educationServices";
 import { useModalStore } from "../../store/modal.store";
 import { storeToRefs } from "pinia";
